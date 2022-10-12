bda50a5beaeeca2d50a43f77ec8ba2ac7473c761
dannywinter22 <69557137+dannywinter22@users.noreply.github.com>
2022-10-12 09:01:36 -0700

    Version1.4
    Home

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ menu.onclick = () => {
     menu.classList.toggle('fa-time');
     navbar.classList.toggle('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
